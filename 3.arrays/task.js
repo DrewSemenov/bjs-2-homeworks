d9b9802f045a340349a55b81cedde5c6c246139d
Andrei Semenov <drew.semenow@gmail.com>
2023-10-04 09:31:49 +0300

    All tasks completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,26 @@
+"use strict";
+
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  return arr1.every((el, i) => el === arr2[i]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  if (!users.length) {
+    return 0;
+  }
+
+  const result = users
+    .filter((a) => a.gender === gender)
+    .reduce((p, c, i, arr) => {
+      if (i === arr.length - 1) {
+        return (p + c.age) / arr.length;
+      }
+      return p + c.age;
+    }, 0);
+
+  return result;
 }
