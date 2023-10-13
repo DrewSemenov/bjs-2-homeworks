516520bc12349c48698d3524eeb7601de0e12105
Andrei Semenov <drew.semenow@gmail.com>
2023-10-13 11:40:06 +0300

    All done

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,44 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+  return (...args) => {
+    const hash = md5(args);
+
+    const searchInCache = cache.find((el) => el[hash]);
+
+    if (searchInCache) {
+      return `Из кеша: ${searchInCache.result}`;
+    } else {
+      const result = func(...args);
+      cache.push({ [hash]: hash, result });
+      cache = cache.splice(-5);
+      return `Вычисляем: ${result}`;
+    }
+  };
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+  let timeout;
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  function wrapper(...args) {
+    if (!timeout) {
+      func.apply(this, args);
+      wrapper.count++;
+    }
+
+    clearTimeout(timeout);
+    wrapper.allCount++;
+
+    timeout = setTimeout(() => {
+      func.apply(this, args);
+      wrapper.count++;
+    }, delay);
+  }
+
+  return wrapper;
 }
+
